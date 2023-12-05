9aa2e77d260b474d7b2d0479cf5e85763d229ad0
nastsumican <shinzi7280.18@gmail.com>
2023-12-05 16:17:46 +0000

    routing 修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,33 +13,25 @@ Rails.application.routes.draw do
   end
   
   devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "customer/registrations",
-    sessions: "customer/sessions"
+    registrations: "public/registrations",
+    sessions: "public/sessions"
   }
   
-  root to: 'customer/homes#top'
-  
-  get '/customers/my_page' => 'customer/customers#show', as: :my_page
-  get '/customers/information/edit' => 'customer/customers#edit', as: :my_information_edit
-  patch '/customers/information' => 'customer/customers#update', as: :my_information
-  get '/customers/information' => 'customer/customers#edit'
-  get '/customers/unsubscribe' => 'customer/customers#unsubscribe', as: :unsubscribe
-  patch 'customer/withdraw' => 'customer/customers#withdraw', as: :withdraw
-  
-  
-  namespace :customer do
+  scopr module: :public do
+    root to: 'homes#top'
+    get '/customers/my_page' => 'customers#show', as: :my_page
+    get '/customers/information/edit' => 'customers#edit', as: :my_information_edit
+    patch '/customers/information' => 'customers#update', as: :my_information
+    get '/customers/unsubscribe' => 'customers#unsubscribe', as: :unsubscribe
+    patch 'customer/withdraw' => 'customers#withdraw', as: :withdraw
+    
     resources :items, only: [:index, :show]
     resources :cart_items, except: [:new, :show, :edit]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all', as: :customer_cart_item_destroy
     resources :addresses, except: [:new, :show]
-    
     resources :orders, except: [:edit, :update, :destroy]
     post '/order/confirm' => 'orders#confirm'
     get '/order/complete' => 'orders#complete'
   end
   
-  delete '/customer/cart_items' => 'customer/cart_items#destroy_all', as: :customer_cart_item_destroy
-  
-  
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
