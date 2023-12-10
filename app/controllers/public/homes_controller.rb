e86d85120fc843b0ef30553cf37b446109779dae
nastsumican <shinzi7280.18@gmail.com>
2023-12-10 16:53:17 +0000

    ジャンル検索機能の修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @items = Item.order(id: "DESC").where(is_active: true).first(4)
-    @item_type = "全て"
+    @genre_type = "全て"
     @item_count = Item.where(is_active: true).last(4).count
     @genres = Genre.all
   end
